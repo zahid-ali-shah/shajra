8386f2dab5b5e4ca1a9db543bfb84e8d3c781d79
zahid ali <zahid.ali@arbisoft.com>
2022-10-13 21:58:45 +0500

    Adding ansaab app

---
+++ shajra/ansaab/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+
+app_name = "ansaab"
+urlpatterns = [
+    # path("~redirect/", view=user_redirect_view, name="redirect"),
+    # path("~update/", view=user_update_view, name="update"),
+    # path("<str:username>/", view=user_detail_view, name="detail"),
+]
