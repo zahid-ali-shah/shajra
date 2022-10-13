2a180f11133ca378143aebd276d24b24665d402a
zahid ali <zahid.ali@arbisoft.com>
2022-10-13 22:00:22 +0500

    Removed ignored files

--- shajra/ansaab/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-
-app_name = "ansaab"
-urlpatterns = [
-    # path("~redirect/", view=user_redirect_view, name="redirect"),
-    # path("~update/", view=user_update_view, name="update"),
-    # path("<str:username>/", view=user_detail_view, name="detail"),
-]
